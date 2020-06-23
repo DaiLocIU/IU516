e2f490990125db1d4fd35a94fbd987da4092de95
DaiLocIU <62388836+DaiLocIU@users.noreply.github.com>
2020-06-23 18:59:18 +0700

    Updates

---
+++ precache-manifest.3b04011f824c6319b376abf862fff69b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cbb102e6f8ad85634147a153819d11",
+    "url": "/DaiLocIU.github.io/index.html"
+  },
+  {
+    "revision": "174f007ce49ec670d9d9",
+    "url": "/DaiLocIU.github.io/static/css/main.aee6717e.chunk.css"
+  },
+  {
+    "revision": "a374b470f4e153e5524b",
+    "url": "/DaiLocIU.github.io/static/js/2.0cf2fde1.chunk.js"
+  },
+  {
+    "revision": "3b91f545d9cac3c7c020d8f89e6757ee",
+    "url": "/DaiLocIU.github.io/static/js/2.0cf2fde1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174f007ce49ec670d9d9",
+    "url": "/DaiLocIU.github.io/static/js/main.743ac8d7.chunk.js"
+  },
+  {
+    "revision": "7a314f5789c363322f67",
+    "url": "/DaiLocIU.github.io/static/js/runtime-main.24d26bdf.js"
+  },
+  {
+    "revision": "33fbc7df96ecc8a38d8c13b4113ed047",
+    "url": "/DaiLocIU.github.io/static/media/banner_april2020.33fbc7df.jpg"
+  },
+  {
+    "revision": "f486e255dff057f999a4e7734ee853ec",
+    "url": "/DaiLocIU.github.io/static/media/banner_eight.f486e255.jpg"
+  },
+  {
+    "revision": "42062313c5865dbdfb668a6db5956fcc",
+    "url": "/DaiLocIU.github.io/static/media/banner_lovepoem.42062313.jpg"
+  },
+  {
+    "revision": "57384e3570b9fdc6450b39dd5497050e",
+    "url": "/DaiLocIU.github.io/static/media/main_visual1.57384e35.jpg"
+  },
+  {
+    "revision": "09f76d4b9d695f56e76953699d117c1b",
+    "url": "/DaiLocIU.github.io/static/media/main_visual2.09f76d4b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a614053059b9262f2efe043ce6a48ef.js"
+  "/DaiLocIU.github.io/precache-manifest.3b04011f824c6319b376abf862fff69b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DaiLocIU.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
